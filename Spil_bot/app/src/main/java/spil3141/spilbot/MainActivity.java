5122e86fd35966e727bd7e17b7b37b63ebb45b1d
Seung-Pil Coleman <spil3141@naver.com>
2019-09-22 01:13:30 +0900

    Backup

--- Spil_bot/app/src/main/java/spil3141/spilbot/MainActivity.java
+++ Spil_bot/app/src/main/java/spil3141/spilbot/MainActivity.java
@@ -1,14 +1,126 @@
 package spil3141.spilbot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    Button btn_2;
+    WebView myWebView = (WebView) findViewById(R.id.webview);
+    //######################################################
+    final AppCompatActivity thisActivaity = this;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    //######################################################
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn_2 = findViewById(R.id.btn_2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivaity,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(thisActivaity,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"Permission is granted",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check if Internet Permission is granted
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivaity,
+                            Manifest.permission.INTERNET)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(thisActivaity,
+                                new String[]{Manifest.permission.INTERNET},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }else{
+                    //final TextView textView = (TextView) findViewById(R.id.textView1);
+
+
+                    // Instantiate the RequestQueue.
+                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    String url ="http://sitex.iptime.org";
+
+                    // Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    // Display the first 500 characters of the response string.
+                                    //textView.setText("Response is: "+ response);
+                                    myWebView.loadData(response, "text/html", "UTF-8");
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //textView.setText("That didn't work!");
+                        }
+                    });
+                    // Add the request to the RequestQueue.
+                    queue.add(stringRequest);
+                }
+            }
+        });
+
     }
 }
